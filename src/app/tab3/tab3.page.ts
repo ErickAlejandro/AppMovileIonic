4c1653b380f8a125ada9eca3ad260c22a16672ba
Erick Ayala <acea1234@hotmail.com>
2021-07-31 17:28:30 -0500

    Segundo Commit Storage

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@capacitor/storage';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  items ={
+    key: '',
+    value: ''
+  };
+
+  key: string;
+  value: string;
+  ver: any;
   constructor() {}
 
+  saveStorage() {
+    Storage.set({
+      key:this.key,
+      value:this.value
+    }).then(()=>{
+      alert('Storage guardado con Exito');
+    });
+  }
+
+  getStorage(){
+    Storage.get({
+      key:this.key
+    }).then((value)=>{
+      alert('Valor de Llave' + this.key + 'es: ' + value.value);
+    });
+  }
+
+  removeStorage(){
+    Storage.remove({
+      key:this.key
+    }).then(()=>{
+      alert('Valor de Llave' + this.key + 'fue borrado: ');
+    });
+  }
 }
